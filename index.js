3a141dd27ef7e06de2a7dc5537cc7bf4c42e8feb
Danny Spangenberg <danny.spangenberg@twiceware.de>
2026-03-27 11:18:02 +0100

    More tets

--- index.js
+++ index.js
@@ -1,10 +1,107 @@
 'use strict'
+const axios = require('axios')
+const simpleParser = require('mailparser').simpleParser
+const stringify = require('string.ify')
+const DSN = require('haraka-dsn')
 
 exports.register = function () {
   this.load_dropbox_ini()
 
   // register hooks here. More info at https://haraka.github.io/core/Plugins/
-  // this.register_hook('data_post', 'do_stuff_with_message')
+  this.register_hook('rcpt', 'check_rcpt')
+  this.register_hook('data', 'parse_body')
+  this.register_hook('data_post', 'post_to_dropbox')
+}
+
+exports.check_rcpt = function (next, connection, params) {
+  const rcpt_to = `${params[0].user}@${params[0].host}`
+  const url = this.cfg.dropboxes[rcpt_to]
+  if (url) {
+    next(OK)
+  } else {
+    next(DENY, DSN.no_such_user())
+  }
+}
+
+exports.parse_body = function (next, connection) {
+  connection.transaction.parse_body = true
+  return next()
+}
+
+exports.post_to_dropbox = function (next, connection) {
+  const plugin = this
+  plugin.loginfo('hook_data_post called')
+
+  if (!connection.transaction.message_stream) {
+    plugin.logerror('No message_stream available')
+    return next()
+  }
+
+  plugin.loginfo(
+    'message_stream type: ' + typeof connection.transaction.message_stream,
+  )
+  plugin.loginfo(
+    'message_stream readable: ' +
+      connection.transaction.message_stream.readable,
+  )
+
+  simpleParser(connection.transaction.message_stream, (err, mail) => {
+    if (err) {
+      plugin.logerror('simpleParser error: ' + err)
+      return next()
+    }
+
+    plugin.loginfo('simpleParser completed')
+    plugin.loginfo('E-Mail: ' + stringify(mail))
+
+    const messageId = mail.messageId || Date.now() + '@haraka'
+
+    let rcpt_to
+
+    if (
+      !!connection.transaction.rcpt_to &&
+      connection.transaction.rcpt_to.length > 0
+    ) {
+      const rcpt = connection.transaction.rcpt_to[0]
+      rcpt_to = `${rcpt.user}@${rcpt.host}`
+    }
+
+    plugin.loginfo('rcpt_to', rcpt_to)
+
+    const url = plugin.cfg.dropboxes[rcpt_to]
+    plugin.loginfo('Dropbox ', url)
+    if (url) {
+      const _email = {
+        from: mail.from.value.map((item) => item.address),
+        to: mail.to.value.map((item) => item.address),
+        rcpt_to: rcpt_to,
+        cc: mail.cc,
+        bcc: mail.bcc,
+        subject: mail.subject,
+        message_id: messageId,
+        attachments: mail.attachments || [],
+        html: mail.html,
+        text: mail.text,
+        textAsHtml: mail.textAsHtml,
+        timestamp: new Date(),
+        references: mail.references || [],
+      }
+
+      if (!!mail.inReplyTo && mail.inReplyTo.length > 0)
+        _email.in_reply_to = mail.inReplyTo[0]
+      else _email.in_reply_to = false
+
+      plugin.loginfo('Processed E-Mail: ' + stringify(_email))
+
+      axios.post(url, {
+        payload: _email,
+      })
+
+      next(OK)
+    } else {
+      next(DENY, DSN.no_such_user())
+    }
+  })
 }
 
 exports.load_dropbox_ini = function () {
--- test/index.js
+++ test/index.js
@@ -1,21 +1,48 @@
+'use strict'
+const path = require('node:path')
 const assert = require('node:assert/strict')
-const { beforeEach, describe, it } = require('node:test')
+const { Readable } = require('node:stream')
+const { afterEach, beforeEach, describe, it, mock } = require('node:test')
 
 // npm modules
 const fixtures = require('haraka-test-fixtures')
+const axios = require('axios')
 
 // start of tests
 //    assert: https://nodejs.org/api/assert.html
 
 beforeEach(() => {
-  this.plugin = new fixtures.plugin('dropbox')
-  
+  this.plugin = new fixtures.plugin(path.join(__dirname, '..'))
+
   // replace vm-compiled fns with instrumented copies for coverage tracking
   if (process.env.HARAKA_COVERAGE) {
     const plugin_module = require('../index.js')
     Object.assign(this.plugin, plugin_module)
   }
+
+  this.connection = fixtures.connection.createConnection({})
+  this.connection.init_transaction()
 })
+
+afterEach(() => {
+  mock.restoreAll()
+})
+
+const RAW_EMAIL = [
+  'From: sender@example.com',
+  'To: test@example.com',
+  'Subject: Test Email',
+  'Message-ID: <test123@example.com>',
+  '',
+  'Hello World',
+].join('\r\n')
+
+function createEmailStream(rawEmail) {
+  const stream = new Readable({ read() {} })
+  stream.push(Buffer.from(rawEmail))
+  stream.push(null)
+  return stream
+}
 
 describe('register', () => {
   it('has a register function', () => {
@@ -29,8 +56,11 @@ describe('register', () => {
         enabled: true,
       },
       feature_section: {
-        yes: true
-      }
+        yes: true,
+      },
+      dropboxes: {
+        'dropbox@example.com': 'https://',
+      },
     }
 
     assert.deepEqual(this.plugin.cfg, undefined)
@@ -42,7 +72,6 @@ describe('register', () => {
 describe('load_dropbox_ini', () => {
   it('loads', () => {
     assert.equal('object', typeof this.plugin)
-    assert.equal('dropbox', this.plugin.name)
   })
 
   it('loads dropbox.ini from config/dropbox.ini', () => {
@@ -66,5 +95,127 @@ describe('uses text fixtures', () => {
     this.connection = fixtures.connection.createConnection({})
     this.connection.init_transaction()
     assert.ok(this.connection.transaction.header)
+  })
+})
+
+describe('check_rcpt', () => {
+  it('calls next(OK) when rcpt is found in dropboxes', () => {
+    this.plugin.cfg = { dropboxes: { 'user@example.com': 'https://dropbox.example.com' } }
+    let called_with
+    this.plugin.check_rcpt(
+      (code) => {
+        called_with = code
+      },
+      this.connection,
+      [{ user: 'user', host: 'example.com' }],
+    )
+    assert.equal(called_with, OK)
+  })
+
+  it('calls next(DENY) when rcpt is not in dropboxes', () => {
+    this.plugin.cfg = { dropboxes: {} }
+    let called_with
+    this.plugin.check_rcpt(
+      (code) => {
+        called_with = code
+      },
+      this.connection,
+      [{ user: 'unknown', host: 'example.com' }],
+    )
+    assert.equal(called_with, DENY)
+  })
+})
+
+describe('parse_body', () => {
+  it('sets transaction.parse_body to true', () => {
+    this.plugin.parse_body(() => {}, this.connection)
+    assert.equal(this.connection.transaction.parse_body, true)
+  })
+
+  it('calls next', () => {
+    let called = false
+    this.plugin.parse_body(
+      () => {
+        called = true
+      },
+      this.connection,
+    )
+    assert.ok(called)
+  })
+})
+
+describe('post_to_dropbox', () => {
+  it('calls next immediately when no message_stream', (t, done) => {
+    this.connection.transaction.message_stream = null
+    this.plugin.post_to_dropbox(done, this.connection)
+  })
+
+  it('calls next when simpleParser errors', (t, done) => {
+    const errStream = new Readable({ read() {} })
+    errStream.readable = true
+    this.connection.transaction.message_stream = errStream
+    this.plugin.post_to_dropbox(() => done(), this.connection)
+    process.nextTick(() => errStream.emit('error', new Error('parse error')))
+  })
+
+  it('calls next(OK) and posts to dropbox when rcpt matches', (t, done) => {
+    const axiosMock = mock.method(axios, 'post', () => Promise.resolve({}))
+    this.connection.transaction.message_stream = createEmailStream(RAW_EMAIL)
+    this.connection.transaction.rcpt_to = [{ user: 'test', host: 'example.com' }]
+    this.plugin.cfg = { dropboxes: { 'test@example.com': 'https://dropbox.example.com' } }
+
+    this.plugin.post_to_dropbox((code) => {
+      assert.equal(code, OK)
+      assert.equal(axiosMock.mock.calls.length, 1)
+      assert.equal(axiosMock.mock.calls[0].arguments[0], 'https://dropbox.example.com')
+      done()
+    }, this.connection)
+  })
+
+  it('calls next(DENY) when rcpt has no matching dropbox', (t, done) => {
+    this.connection.transaction.message_stream = createEmailStream(RAW_EMAIL)
+    this.connection.transaction.rcpt_to = [{ user: 'unknown', host: 'example.com' }]
+    this.plugin.cfg = { dropboxes: {} }
+
+    this.plugin.post_to_dropbox((code) => {
+      assert.equal(code, DENY)
+      done()
+    }, this.connection)
+  })
+
+  it('sets in_reply_to when In-Reply-To header is present', (t, done) => {
+    const rawEmailWithReply = [
+      'From: sender@example.com',
+      'To: test@example.com',
+      'Subject: Re: Test',
+      'Message-ID: <reply@example.com>',
+      'In-Reply-To: <original@example.com>',
+      '',
+      'Reply body',
+    ].join('\r\n')
+
+    mock.method(axios, 'post', (url, body) => {
+      assert.notEqual(body.payload.in_reply_to, false)
+      done()
+      return Promise.resolve({})
+    })
+
+    this.connection.transaction.message_stream = createEmailStream(rawEmailWithReply)
+    this.connection.transaction.rcpt_to = [{ user: 'test', host: 'example.com' }]
+    this.plugin.cfg = { dropboxes: { 'test@example.com': 'https://dropbox.example.com' } }
+    this.plugin.post_to_dropbox(() => {}, this.connection)
+  })
+
+  it('sets in_reply_to to false when In-Reply-To header is absent', (t, done) => {
+    mock.method(axios, 'post', (url, body) => {
+      assert.equal(body.payload.in_reply_to, false)
+      done()
+      return Promise.resolve({})
+    })
+
+    this.connection.transaction.message_stream = createEmailStream(RAW_EMAIL)
+    this.connection.transaction.rcpt_to = [{ user: 'test', host: 'example.com' }]
+    this.plugin.cfg = { dropboxes: { 'test@example.com': 'https://dropbox.example.com' } }
+    this.plugin.post_to_dropbox(() => {}, this.connection)
   })
 })
